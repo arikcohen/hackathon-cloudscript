c101a1055d6d8cc7827f11126f7ba682f4696d52
Thomas Gutschmidt <thomg@microsoft.com>
2018-10-31 12:23:34 -0700

    Checking in lost files
    Trying to fix merge conflicts

--- projects/authentication/trading.ts
+++ projects/authentication/trading.ts
@@ -8,6 +8,7 @@ var TradeHelloWorld = function (args: any, context: IPlayFabContext): IAuthentic
     // The pre-defined "currentPlayerId" variable is initialized to the PlayFab ID of the player logged-in on the game client. 
     // Cloud Script handles authenticating the player automatically.
     var message = "Hello " + currentPlayerId + "!";
+    var message = "Checking Inventory for " + currentPlayerId + "!";
 
     // You can use the "log" object to write out debugging statements. It has
     // three functions corresponding to logging level: debug, info, and error. These functions
