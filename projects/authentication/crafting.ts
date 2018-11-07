93374f8e835fe78987e7bd82113405ee9a069ca5
Thomas Gutschmidt <thomg@microsoft.com>
2018-11-07 13:30:49 -0800

    Crafting clean up
    Moved Javascript to TS syntax

---
+++ projects/authentication/crafting.ts
@@ -0,0 +1,28 @@
+﻿handlers.craft = function (args, context) {
+    var message = "Crafting initiated by" + currentPlayerId + "dude!";
+    log.info(message);
+
+    //Inventory item to Craft
+    var rewards = "Boots_Speed";
+
+    //Currency costs to crafted item
+    var craftCostinGold = 15;
+    var craftCostinGems = 15;
+
+    //subtract currencies
+    server.SubtractUserVirtualCurrency({ PlayFabId: currentPlayerId, VirtualCurrency: "GD", Amount: craftCostinGold });
+    server.SubtractUserVirtualCurrency({ PlayFabId: currentPlayerId, VirtualCurrency: "GM", Amount: craftCostinGems });
+
+    //Add Craft item to invnetory
+    var itemGrantResult = server.GrantItemsToUser(
+        {
+            PlayFabId: currentPlayerId,
+            Annotation: "Given for crafting",
+            ItemIds: [rewards]
+        });
+
+    var resultItems = itemGrantResult.ItemGrantResults;
+
+    return { rewards: resultItems };
+
+};
